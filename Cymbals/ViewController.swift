86baf699da27aae7e92d2cd3b8d79d8238c4751d
Ricardo Hernandez <a.ricardohg@gmail.com>
2015-12-15 17:20:52 -0500

    added .gitignore + project files

---
+++ Cymbals/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Cymbals
+//
+//  Created by ricardo hernandez  on 12/15/15.
+//  Copyright © 2015 ricardo hernandez . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
