b8bd64254efea5824049cd27cc67e39ce6bb2083
Ricardo Hernandez <a.ricardohg@gmail.com>
2015-12-22 20:03:31 -0500

    removed storyboard, added login vc

---
+++ Cymbals/ViewControllers/LoginViewController.swift
@@ -0,0 +1,17 @@
+//
+//  LoginViewController.swift
+//  Cymbals
+//
+//  Created by ricardo hernandez  on 12/22/15.
+//  Copyright © 2015 ricardo hernandez . All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.title = NSLocalizedString("Login", comment: "Login")
+    }
+}
